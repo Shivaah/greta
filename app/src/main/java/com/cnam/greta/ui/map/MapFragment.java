ddcf51cea303351968b6d12316d24118e6731176
Paul <paul_michels@hotmail.fr>
2020-05-18 19:02:49 +0200

    Added settings

--- app/src/main/java/com/cnam/greta/ui/map/MapFragment.java
+++ app/src/main/java/com/cnam/greta/ui/map/MapFragment.java
@@ -1,11 +1,14 @@
 package com.cnam.greta.ui.map;
 
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +17,13 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceManager;
 
 import com.cnam.greta.R;
 import com.cnam.greta.model.LocationModel;
 import com.cnam.greta.view.Altimeter;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -30,15 +36,11 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import static android.content.Context.LOCATION_SERVICE;
 
-public class MapFragment extends Fragment implements OnMapReadyCallback, LocationListener {
-
-    private static final int MIN_MILLIS_LOCATION_REQUEST = 5000;
-    private static final int MIN_METERS_LOCATION_REQUEST = 10;
+public class MapFragment extends Fragment{
 
     private View rootView;
 
@@ -61,12 +63,61 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
                     mGoogleMap.addMarker(new MarkerOptions().position(userPosition).title((String) user.getValue().get("username")));
                 }
             }
-            dataSnapshot.getValue();
         }
 
         @Override
         public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.w(this.getClass().getSimpleName(), databaseError.getMessage());
+        }
+    };
 
+    /**
+     * Callback for Google Map initialization
+     */
+    private final OnMapReadyCallback mapReadyCallback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap map) {
+            mGoogleMap = map;
+            mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        }
+    };
+
+    /**
+     * Callback for location changes
+     */
+    private final LocationListener locationListener = new LocationListener() {
+        @SuppressLint("HardwareIds")
+        @Override
+        public void onLocationChanged(Location location) {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+            usersDatabaseReference.child(Settings.Secure.getString(requireActivity().getContentResolver(), Settings.Secure.ANDROID_ID))
+                    .setValue(new LocationModel(
+                            sharedPreferences.getString(requireContext().getString(R.string.username_key), getString(R.string.unknown_user)),
+                            location.getLatitude(),
+                            location.getLongitude(),
+                            location.getAltitude()
+                    ));
+
+            CameraUpdate center= CameraUpdateFactory.newLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
+            CameraUpdate zoom = CameraUpdateFactory.zoomTo(10);
+            mAltimeter.setAltitude((float) location.getAltitude());
+            mGoogleMap.moveCamera(center);
+            mGoogleMap.animateCamera(zoom);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            //Deprecated
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.i(this.getClass().getSimpleName(), "Provider enabled : " + provider);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.i(this.getClass().getSimpleName(), "Provider disabled : " + provider);
         }
     };
 
@@ -79,7 +130,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if(rootView == null){
             rootView = inflater.inflate(R.layout.fragment_map, container, false);
 
@@ -87,7 +138,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
             mAltimeter = rootView.findViewById(R.id.altimeter);
 
             mMapView.onCreate(savedInstanceState);
-            mMapView.getMapAsync(this);
+            mMapView.getMapAsync(mapReadyCallback);
         }
         return rootView;
     }
@@ -98,7 +149,20 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         mMapView.onResume();
         LocationManager locationManager = (LocationManager) requireContext().getSystemService(LOCATION_SERVICE);
         if (locationManager != null && ContextCompat.checkSelfPermission( requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED ) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_MILLIS_LOCATION_REQUEST, MIN_METERS_LOCATION_REQUEST, this);
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext());
+            int minMillis = Integer.parseInt(sharedPreferences.getString(
+                    requireContext().getString(R.string.localisation_update_time_frequency_key),
+                    requireContext().getString(R.string.localisation_update_time_frequency_default)
+            ));
+            int minDistance = Integer.parseInt(sharedPreferences.getString(
+                    requireContext().getString(R.string.localisation_update_distance_frequency_key),
+                    requireContext().getString(R.string.localisation_update_distance_frequency_default)
+            ));
+            String provider = sharedPreferences.getString(
+                    requireContext().getString(R.string.localisation_provider_key),
+                    requireContext().getString(R.string.localisation_providers_default)
+            );
+            locationManager.requestLocationUpdates(provider, minMillis, minDistance, locationListener);
         }
         usersDatabaseReference.addValueEventListener(usersListener);
     }
@@ -109,7 +173,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
         mMapView.onPause();
         LocationManager locationManager = (LocationManager) requireContext().getSystemService(LOCATION_SERVICE);
         if(locationManager != null){
-            locationManager.removeUpdates(this);
+            locationManager.removeUpdates(locationListener);
         }
         usersDatabaseReference.removeEventListener(usersListener);
     }
@@ -124,41 +188,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback, Locatio
     public void onLowMemory() {
         super.onLowMemory();
         mMapView.onLowMemory();
-    }
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-        mGoogleMap = map;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        if (ContextCompat.checkSelfPermission( requireContext(), android.Manifest.permission.ACCESS_FINE_LOCATION ) == PackageManager.PERMISSION_GRANTED ) {
-            mGoogleMap.setMyLocationEnabled(true);
-        }
-    }
-
-    @Override
-    public void onLocationChanged(Location location) {
-        mAltimeter.setAltitude((float) location.getAltitude());
-        usersDatabaseReference.child(Settings.Secure.getString(requireActivity().getContentResolver(), Settings.Secure.ANDROID_ID))
-            .setValue(new LocationModel(
-                    Settings.Secure.getString(requireActivity().getContentResolver(), Settings.Secure.ANDROID_ID),
-                location.getLatitude(),
-                location.getLongitude(),
-                location.getAltitude()
-            ));
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-
     }
 }
